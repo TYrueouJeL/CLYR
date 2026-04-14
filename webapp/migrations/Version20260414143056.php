baf4652678f6de1b0feccfd177835dff6dfc294a
Baltroumark <ylan.moulard@gmail.com>
2026-04-14 16:38:51 +0200

    Gros kaka qui pue

---
+++ webapp/migrations/Version20260414143056.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260414143056 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE chaussette (id INT AUTO_INCREMENT NOT NULL, commentaire LONGTEXT NOT NULL, nom_chaussette VARCHAR(255) NOT NULL, taille VARCHAR(255) DEFAULT NULL, couple TINYINT NOT NULL, date_creation DATETIME DEFAULT NULL, couleur_id INT DEFAULT NULL, relation_id INT DEFAULT NULL, type_id INT DEFAULT NULL, INDEX IDX_86BFA0F8C31BA576 (couleur_id), INDEX IDX_86BFA0F83256915B (relation_id), INDEX IDX_86BFA0F8C54C8C93 (type_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE couleur (id INT AUTO_INCREMENT NOT NULL, nom_couleur VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE marque (id INT AUTO_INCREMENT NOT NULL, nom_marque VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE type (id INT AUTO_INCREMENT NOT NULL, nom_type VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('ALTER TABLE chaussette ADD CONSTRAINT FK_86BFA0F8C31BA576 FOREIGN KEY (couleur_id) REFERENCES couleur (id)');
+        $this->addSql('ALTER TABLE chaussette ADD CONSTRAINT FK_86BFA0F83256915B FOREIGN KEY (relation_id) REFERENCES type (id)');
+        $this->addSql('ALTER TABLE chaussette ADD CONSTRAINT FK_86BFA0F8C54C8C93 FOREIGN KEY (type_id) REFERENCES type (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE chaussette DROP FOREIGN KEY FK_86BFA0F8C31BA576');
+        $this->addSql('ALTER TABLE chaussette DROP FOREIGN KEY FK_86BFA0F83256915B');
+        $this->addSql('ALTER TABLE chaussette DROP FOREIGN KEY FK_86BFA0F8C54C8C93');
+        $this->addSql('DROP TABLE chaussette');
+        $this->addSql('DROP TABLE couleur');
+        $this->addSql('DROP TABLE marque');
+        $this->addSql('DROP TABLE type');
+        $this->addSql('DROP TABLE `user`');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
